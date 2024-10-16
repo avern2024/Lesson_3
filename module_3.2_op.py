41a79fdb9c8149077ddfcbdb98e15f0ded1b6509
avern2024 <avern2020@icloud.com>
2024-10-16 13:07:35 +0700

    Add files via upload

--- module_3.2_op.py
+++ module_3.2_op.py
@@ -6,15 +6,15 @@ def is_valid_email(email):
 
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
     if not is_valid_email(recipient):
-        print('Невозможно отправить письмо с адреса <sender> на адрес <recipient>')
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif not is_valid_email(sender):
-        print('Невозможно отправить письмо с адреса <sender> на адрес <recipient>')
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif sender == recipient:
         print('Нельзя отправить письмо самому себе!')
     elif sender == "university.help@gmail.com":
-        print('Письмо успешно отправлено с адреса <sender> на адрес <recipient>')
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
     else:
-        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>')
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
 
 
 send_email('Это сообщение для проверки связи', 'avern@ya.con')
